c5c9d48352c8bddb14bac565b870649362ef5602
chika-bac <frefg.cmw@gmail.com>
2026-04-08 23:42:25 +0900

    header(sp)を作成

--- js/component/hamburger-menu.js
+++ js/component/hamburger-menu.js
@@ -1,4 +1,20 @@
 /**
  * ハンバーガーメニュー
  */
-export const initializeHamburgerMenu = () => {};
+export const initializeHamburgerMenu = () => {
+  const menu = document.querySelector(".js-header-menu");
+  const openButton = document.querySelector(".js-header-menu-open-button");
+  const closeButton = document.querySelector(".js-header-menu-close-button");
+
+  if (!menu || !openButton || !closeButton) return;
+
+  openButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.showModal();
+  });
+
+  closeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.close();
+  });
+};
