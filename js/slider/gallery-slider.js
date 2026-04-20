43fa42ce8b83f9d194cf35d8a87b0e1ad9f33552
chika-bac <frefg.cmw@gmail.com>
2026-04-20 23:01:02 +0900

    about, recruitページにスライダーを実装

--- js/slider/gallery-slider.js
+++ js/slider/gallery-slider.js
@@ -1,4 +1,39 @@
 /**
  * 画像ギャラリースライダー
  */
-export const initializeCultureGallery = () => {};
+export const initializeCultureGallery = () => {
+  // .js-recruit-culture-gallery がないページでは即 return
+  const galleryElement = document.querySelector(".js-recruit-culture-gallery");
+  if (!galleryElement) return;
+
+  const autoScrollExtension = window.splide?.Extensions;
+  if (!autoScrollExtension) return;
+
+  const recruitCultureGallery = new Splide(".js-recruit-culture-gallery", {
+    type: "loop",
+    perPage: 3,
+    arrows: false,
+    pagination: false,
+    gap: "32rem",
+    padding: { left: "112rem", right: "140rem" },
+    autoScroll: {
+      speed: 1,
+      pauseOnHover: false,
+    },
+    breakpoints: {
+      899: {
+        perPage: 1,
+        gap: "10rem",
+        padding: { left: "10rem", right: "142rem" },
+        autoScroll: {
+          speed: 0.6,
+          pauseOnHover: false,
+        },
+      },
+    },
+  });
+
+  if (!recruitCultureGallery) return;
+
+  recruitCultureGallery.mount(autoScrollExtension);
+};
--- js/slider/staff-slider.js
+++ js/slider/staff-slider.js
@@ -1,4 +1,27 @@
 /**
  * スタッフスライダー
  */
-export const initializeAboutStaffSlider = () => {};
+export const initializeAboutStaffSlider = () => {
+  const aboutStaffSlider = new Splide(".js-about-staff-slider", {
+    type: "loop",
+    perPage: 3,
+    perMove: 1,
+    arrows: false,
+    pagination: false,
+    autoplay: true,
+    interval: 3000,
+    gap: "40rem",
+    padding: { left: "172rem", right: "259rem" },
+    breakpoints: {
+      899: {
+        perPage: 1,
+        gap: "32rem",
+        padding: { left: "20rem", right: "125rem" },
+      },
+    },
+  });
+
+  if (!aboutStaffSlider) return;
+
+  aboutStaffSlider.mount();
+};
